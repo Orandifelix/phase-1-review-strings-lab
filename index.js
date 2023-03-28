0390f5d2e71dc26a3112ec79a3097089b8ef59fc
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-28 10:56:55 +0300

    3rd commit

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
 const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
-const shortGreeting = "Welcome, G !"
+const shortGreeting =  `Welcome, ${currentUser.slice(0, 1)}!`;
