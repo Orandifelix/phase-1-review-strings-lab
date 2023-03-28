1786c0647383b41cb936b3aca383b1d8600ed1b6
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-28 10:50:30 +0300

    second solution

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Write your code in this file!
 
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = ("Welcome to Flatbook, Grace Hopper !");
+// const excitedWelcomeMessage = ("WELCOME TO FLATBOOK, GRACE HOPPER!");
+// const shortGreeting = "Welcome, Grace!"
+
 const currentUser = 'Grace Hopper';
-const welcomeMessage = ("Welcome to Flatbook, Grace Hopper !");
-const excitedWelcomeMessage = ("WELCOME TO FLATBOOK, GRACE HOPPER!");
-const shortGreeting = "Welcome, Grace!"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = "Welcome, G !"
